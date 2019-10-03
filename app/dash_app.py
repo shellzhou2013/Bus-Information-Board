06849b69d66a505d5b054537eb03563df037eb7d
shellzhou2013 <50307699+shellzhou2013@users.noreply.github.com>
2019-10-03 14:35:24 -0700

    Update dash_app.py

--- app/dash_app.py
+++ app/dash_app.py
@@ -22,46 +22,20 @@ app.layout = html.Div([
 ])
 
 @app.callback(
-    Output(component_id='my-div0', component_property='children'),
+    [Output(component_id='my-div0', component_property='children'),
+     Output(component_id='my-div1', component_property='children'),
+     Output(component_id='my-div2', component_property='children'),
+     Output(component_id='my-div3', component_property='children')
+    ],
     [Input(component_id='my-id', component_property='value')]
 )
 def stop_name(input_value):
-    return 'The stop you searched is: ' + get_stop_name(str(input_value))
+    res1 = 'The stop you searched is: ' + get_stop_name(str(input_value))
+    res2 = get_arrival_information_for_stop(str(input_value))
+    res3 = 'Nearby Stop(s): ' + ' '.join(find_nearby_stops(str(input_value)))
+    res4 = combine_nearby_stop_information(str(input_value))
+    return res1, res2, res3, res4
 
 
-@app.callback(
-    Output(component_id='my-div1', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
-)
-def arrival_information(input_value):
-    return get_arrival_information_for_stop(str(input_value))
-
-@app.callback(
-    Output(component_id='my-div2', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
-)
-def nearby_stops(input_value):
-    temp = find_nearby_stops(str(input_value))
-    # add stop name to the stop id
-    for i in range(len(temp)):
-        temp[i] += ' ' + get_stop_name(temp[i])
-    
-    
-    info = ''
-    if len(temp) == 0:
-        info = 'No nearby stops!'
-    elif len(temp) == 1:
-        info = 'The nearby stop is: ' + temp[0]
-    else:
-        info = 'The nearby stops are: ' + '+++'.join(temp)
-    return info
-
-@app.callback(
-    Output(component_id='my-div3', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
-)
-def nearby_information(input_value):
-    return combine_nearby_stop_information(str(input_value))
-    
 if __name__ == '__main__':
     app.run_server(debug=True, host= '0.0.0.0')
